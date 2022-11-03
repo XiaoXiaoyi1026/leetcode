538c67f2ced92607a236b40ba74930cf7926074a
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2022-11-03 12:27:31 +0800

    树形DP初识: 求一棵树上的任意两节点间的最大距离

---
+++ src/main/java/com/xiaoxiaoyi/tree/TreeMaxDistance.java
@@ -0,0 +1,107 @@
+package com.xiaoxiaoyi.tree;
+
+/**
+ * @author xiaoxiaoyi
+ * 树形dp, 求树上任意两节点间的最大距离
+ */
+public class TreeMaxDistance {
+
+    /**
+     * 节点
+     */
+    public static class Node {
+        /**
+         * 左孩子
+         */
+        private Node left;
+        /**
+         * 右孩子
+         */
+        private Node right;
+
+        /**
+         * 初始化
+         */
+        Node() {
+            left = null;
+            right = null;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
+
+    /**
+     * 每颗子树需要返回的信息
+     */
+    private static class Info {
+        /**
+         * 这棵树上的任意两节点间的最大距离
+         */
+        private final int maxDistance;
+        /**
+         * 这棵树的高度
+         */
+        private final int height;
+
+        Info(int maxDistance, int height) {
+            this.maxDistance = maxDistance;
+            this.height = height;
+        }
+
+        public int getMaxDistance() {
+            return maxDistance;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+    }
+
+    /**
+     * @param root 根节点
+     * @return 以root为根节点的二叉树上的任意两个节点间的最大距离
+     */
+    public static int maxDistance(Node root) {
+        return process(root).getMaxDistance();
+    }
+
+    /**
+     * @param root 根节点
+     * @return 以root为根的树的信息
+     */
+    private static Info process(Node root) {
+        if (root == null) {
+            // base case
+            return new Info(0, 0);
+        }
+        // 获取左子树和右子树的信息
+        Info leftInfo = process(root.getLeft());
+        Info rightInfo = process(root.getRight());
+        // 获取左右两个子树上的任意两节点间的最大距离
+        int leftMaxDistance = leftInfo.getMaxDistance();
+        int rightMaxDistance = rightInfo.getMaxDistance();
+        // 求出root参与的情况下的任意两节点间的最大距离
+        int maxDistance = leftInfo.getHeight() + rightInfo.getHeight() + 1;
+        // 求出真正的最大距离
+        maxDistance = Math.max(maxDistance, Math.max(leftMaxDistance, rightMaxDistance));
+        // 求出自己的高度 = 左子树和右子树的最大高度 + 1
+        int height = Math.max(leftInfo.getHeight(), rightInfo.getHeight()) + 1;
+        // 返回自己的信息
+        return new Info(maxDistance, height);
+    }
+
+}
---
+++ src/test/java/com/xiaoxiaoyi/tree/TreeMaxDistanceTest.java
@@ -0,0 +1,28 @@
+package com.xiaoxiaoyi.tree;
+
+import junit.framework.TestCase;
+
+public class TreeMaxDistanceTest extends TestCase {
+
+    public void testTreeDp() {
+        TreeMaxDistance.Node node1 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node2 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node3 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node4 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node5 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node6 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node7 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node8 = new TreeMaxDistance.Node();
+        TreeMaxDistance.Node node9 = new TreeMaxDistance.Node();
+        node1.setLeft(node2);
+        node1.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+        node6.setRight(node7);
+        node5.setLeft(node8);
+        node8.setLeft(node9);
+        System.out.println(TreeMaxDistance.maxDistance(node1));
+    }
+
+}
