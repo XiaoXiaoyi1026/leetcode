67396cb0e85ef53c434062f51eb761630bec65a3
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2022-11-27 15:47:37 +0800

    数字字符串转字母串的方式数, dp优化

--- src/main/java/com/xiaoxiaoyi/recursion/ConvertToLetterString.java
+++ src/main/java/com/xiaoxiaoyi/recursion/ConvertToLetterString.java
@@ -8,48 +8,128 @@ package com.xiaoxiaoyi.recursion;
  */
 public class ConvertToLetterString {
 
-    public static int convertToLetterString(String numberString) {
-        char[] numberChars = numberString.toCharArray();
+    public static int convertToLetterString(int num) {
+        if (num < 1) {
+            return 0;
+        }
+        char[] str = String.valueOf(num).toCharArray();
         // 从numberChars的0位置开始遍历所有可能
-        return process(numberChars, 0);
+        return process(str, 0);
     }
 
     /**
      * 递归遍历numberChars上的每一种可能
      *
-     * @param numberChars 数字
-     * @param i 当前遍历到的位置
+     * @param str   数字
+     * @param index 当前遍历到的位置
      * @return 可能的组合数
      */
-    private static int process(char[] numberChars, int i) {
-        if (i == numberChars.length) {
-            // i == numberChars.length时，说明前面0~i - 1都已经做好了决定，即有1种可能
+    private static int process(char[] str, int index) {
+        if (index == str.length) {
+            // index == str.length时，说明前面0~index - 1都已经做好了决定，即有1种可能
             return 1;
         }
-        if (numberChars[i] == '0') {
+        if (str[index] == '0') {
             // 当前数字'0'不能自己包括和后续任何字符去匹配字母
             return 0;
         }
-        if (numberChars[i] == '1') {
+        int n = str.length;
+        if (str[index] == '1') {
             // 自己作为单独的部分后续的可能
-            int res = process(numberChars, i + 1);
-            if (i + 1 < numberChars.length) {
+            int res = process(str, index + 1);
+            if (index + 1 < n) {
                 // 自己和后续的那个一起匹配一个字符的可能
-                res += process(numberChars, i + 2);
+                res += process(str, index + 2);
             }
             return res;
         }
-        if (numberChars[i] == '2') {
+        if (str[index] == '2') {
             // 自己可以作为单独的部分
-            int res = process(numberChars, i + 1);
+            int res = process(str, index + 1);
             // 如果要和后续的一起，那么后续的那个字符要在0~6之间
-            if (i + 1 < numberChars.length && numberChars[i + 1] >= '0' && numberChars[i + 1] <= '6') {
-                res += process(numberChars, i + 2);
+            if (index + 1 < n && str[index + 1] >= '0' && str[index + 1] <= '6') {
+                res += process(str, index + 2);
             }
             return res;
         }
         // 如果当前字符在'3'~'9'，则只能自己匹配
-        return process(numberChars, i + 1);
+        return process(str, index + 1);
     }
 
+    public static int convertToLetterString2(int num) {
+        if (num < 1) {
+            return 0;
+        }
+        char[] str = String.valueOf(num).toCharArray();
+        // 从numberChars的0位置开始遍历所有可能
+        return process2(str, 0);
+    }
+
+    private static int process2(char[] str, int index) {
+        if (index == str.length) {
+            return 1;
+        } else if (str[index] == '0') {
+            return 0;
+        }
+        // 自己为一个字母
+        int n = str.length, ans = process2(str, index + 1);
+        // 如果还有后续字符且和后续字符的组合小于27
+        if (index != n - 1 && (str[index] - '0') * 10 + (str[index + 1] - '0') < 27) {
+            // 和后面的字符组成一个字母
+            ans += process2(str, index + 2);
+        }
+        return ans;
+    }
+
+    public static int dpProcess(int num) {
+        if (num < 1) {
+            return 0;
+        }
+        char[] str = String.valueOf(num).toCharArray();
+        int n = str.length;
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        if (str[n - 1] != '0') {
+            // 如果最后一个字符不是0, 则dp[n - 1]就等于1
+            dp[n - 1] = 1;
+        }
+        // 根据依赖关系从后往前求结果
+        for (int index = n - 2; index >= 0; index--) {
+            char cur = str[index];
+            if (cur != '0') {
+                dp[index] += dp[index + 1];
+                if ((cur - '0') * 10 + (str[index + 1] - '0') < 27) {
+                    // 前面的预处理是为了这里不出错
+                    dp[index] += dp[index + 2];
+                }
+            }
+        }
+        return dp[0];
+    }
+
+    public static int dpProcess2(int num) {
+        if (num < 1) {
+            return 0;
+        }
+        char[] str = String.valueOf(num).toCharArray();
+        // cur记录当前位置, next记录cur + 1的结果, behindNext记录cur + 2的结果
+        int n = str.length, cur = 0, next = 0, behindNext = 1;
+        if (str[n - 1] != '0') {
+            next = 1;
+        }
+        for (int index = n - 2; index >= 0; index--) {
+            char curChar = str[index];
+            cur = 0;
+            if (curChar != '0') {
+                cur += next;
+                if ((curChar - '0') * 10 + (str[index + 1] - '0') < 27) {
+                    cur += behindNext;
+                }
+            }
+            // 全部前移
+            behindNext = next;
+            next = cur;
+        }
+        return cur;
+    }
 }
--- src/test/java/com/xiaoxiaoyi/recursion/ConvertToLetterStringTest.java
+++ src/test/java/com/xiaoxiaoyi/recursion/ConvertToLetterStringTest.java
@@ -5,7 +5,23 @@ import junit.framework.TestCase;
 public class ConvertToLetterStringTest extends TestCase {
 
     public void testConvertToLetterString() {
-        System.out.println(ConvertToLetterString.convertToLetterString("2938193"));
+        int testTimes = 100;
+        for (int i = 0; i < testTimes; i++) {
+            int num = 20000 + (int) (Math.random() * 30000);
+            int ans1 = ConvertToLetterString.convertToLetterString(num);
+            int ans2 = ConvertToLetterString.convertToLetterString2(num);
+            int ans3 = ConvertToLetterString.dpProcess(num);
+            int ans4 = ConvertToLetterString.dpProcess2(num);
+            if (ans1 != ans2 || ans2 != ans3 || ans3 != ans4) {
+                System.out.println("ooops!");
+                System.out.println(num);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                System.out.println(ans4);
+                break;
+            }
+        }
     }
 
 }
