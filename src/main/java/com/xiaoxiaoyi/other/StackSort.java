efe3cd21b26dc22e7da52f5ad239b397bfab5d08
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2022-11-27 11:24:05 +0800

    栈排序

---
+++ src/main/java/com/xiaoxiaoyi/other/StackSort.java
@@ -0,0 +1,31 @@
+package com.xiaoxiaoyi.other;
+
+import java.util.Stack;
+
+/**
+ * @author xiaoxiaoyi
+ * 给你一个乱序数组, 在栈中, 要求只用1个栈结构, 给原来栈中的元素做一个排序
+ */
+public class StackSort {
+
+    public static void stackSort(Stack<Integer> stack) {
+        // 辅助栈
+        Stack<Integer> help = new Stack<>();
+        while (!stack.isEmpty()) {
+            Integer cur = stack.pop();
+            if (help.isEmpty()) {
+                help.push(cur);
+            } else {
+                if (cur < help.peek()) {
+                    help.push(cur);
+                } else {
+                    stack.push(help.pop());
+                    stack.push(cur);
+                }
+            }
+        }
+        while (!help.isEmpty()) {
+            stack.push(help.pop());
+        }
+    }
+}
---
+++ src/test/java/com/xiaoxiaoyi/other/StackSortTest.java
@@ -0,0 +1,21 @@
+package com.xiaoxiaoyi.other;
+
+import junit.framework.TestCase;
+
+import java.util.Stack;
+
+public class StackSortTest extends TestCase {
+
+    public void testStackSort() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(4);
+        stack.push(1);
+        stack.push(6);
+        stack.push(3);
+        stack.push(5);
+        StackSort.stackSort(stack);
+        System.out.println(stack);
+    }
+
+}
