541b647d09938cc5d7ca41d06978bd02b22a95da
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-01-10 14:48:30 +0800

    LFU缓存(Coding)

--- src/main/java/com/xiaoxiaoyi/leetcode/LFUCache.java
+++ src/main/java/com/xiaoxiaoyi/leetcode/LFUCache.java
@@ -155,65 +155,12 @@ public class LFUCache<K extends Comparable<K>, V> {
         // 从缓存map中取出要set的对象(O(1))
         Element<K, V> setElement = elementMap.get(key);
         if (setElement != null) {
-            // 如果之前已经加入过该对象, 先获取它所在的bucket
-            Bucket<K, V> previousBucket = bucketMap.get(setElement.operations);
-            // 从桶中移除该元素
-            previousBucket.remove(setElement);
-            // 该元素的操作次数+1
-            setElement.operations++;
-            // 判断操作次数+1的桶存不存在
-            Bucket<K, V> nextBucket = bucketMap.get(setElement.operations);
-            if (nextBucket != null) {
-                // 如果存在, 则将该元素加到桶最后去
-                nextBucket.addLast(setElement);
-            } else {
-                // 如果该桶不存在, 则新建
-                nextBucket = new Bucket<>(setElement.operations);
-                nextBucket.addLast(setElement);
-                // 插入新桶, 保证升序
-                previousBucket.nextLink(nextBucket);
-                if (last == previousBucket) {
-                    last = nextBucket;
-                }
-                // bucketMap记录
-                bucketMap.put(setElement.operations, nextBucket);
-            }
+            // 如果之前加入过该对象, 则更新其value
             setElement.value = value;
+            operation(setElement);
         } else {
             // 之前没加入过该对象, 则新建该对象, 并设置好操作次数和最后操作时间
-            setElement = new Element<>(key, value);
-            // 判断缓存满没满
-            if (size < maxSize) {
-                // 如果缓存没满, 将新加入的对象put到elementMap中去
-                elementMap.put(key, setElement);
-                size++;
-                // 加入对应的bucket
-                Bucket<K, V> bucket = bucketMap.get(1);
-                if (bucket == null) {
-                    bucket = new Bucket<>(1);
-                    if (!isEmpty()) {
-                        first.previousLink(bucket);
-                    }
-                    first = bucket;
-                    bucketMap.put(1, bucket);
-                }
-                bucket.addLast(setElement);
-            } else {
-                /*
-                缓存满了, 根据规则删除数据, 先拿到操作次数最少的bucket(first)
-                elementMap删除桶中第一个元素
-                 */
-                elementMap.remove(first.first.key);
-                if (first.onlyOne()) {
-                    // 桶里只有1个元素, 直接删除这个桶即可
-                    removeBucket(first);
-                } else {
-                    // 桶里不止一个元素, 则删除桶里的第一个元素
-                    first.remove(first.first);
-                }
-                size--;
-                set(key, value);
-            }
+            add(new Element<>(key, value));
         }
     }
 
@@ -222,7 +169,75 @@ public class LFUCache<K extends Comparable<K>, V> {
         if (element == null) {
             return null;
         }
+        operation(element);
         return element.value;
+    }
+
+    protected void add(Element<K, V> element) {
+        // 判断缓存满没满
+        if (size < maxSize) {
+            // 如果缓存没满, 将新加入的对象put到elementMap中去
+            elementMap.put(element.key, element);
+            size++;
+            // 加入对应的bucket
+            Bucket<K, V> bucket = bucketMap.get(1);
+            if (bucket == null) {
+                bucket = new Bucket<>(1);
+                if (!isEmpty()) {
+                    first.previousLink(bucket);
+                }
+                first = bucket;
+                bucketMap.put(1, bucket);
+            }
+            bucket.addLast(element);
+        } else {
+            /*
+            缓存满了, 根据规则删除数据, 先拿到操作次数最少的bucket(first)
+            elementMap删除桶中第一个元素
+             */
+            elementMap.remove(first.first.key);
+            if (first.onlyOne()) {
+                // 桶里只有1个元素, 直接删除这个桶即可
+                removeBucket(first);
+                first = first.next;
+            } else {
+                // 桶里不止一个元素, 则删除桶里的第一个元素
+                first.remove(first.first);
+            }
+            size--;
+            add(element);
+        }
+    }
+
+    protected void operation(Element<K, V> element) {
+        // 先获取它所在的bucket
+        Bucket<K, V> previousBucket = bucketMap.get(element.operations);
+        // 该元素的操作次数+1
+        element.operations++;
+        // 判断操作次数+1的桶存不存在
+        Bucket<K, V> nextBucket = bucketMap.get(element.operations);
+        if (nextBucket != null) {
+            // 如果存在, 则将该元素加到桶最后去
+            nextBucket.addLast(element);
+        } else {
+            // 如果该桶不存在, 则新建
+            nextBucket = new Bucket<>(element.operations);
+            nextBucket.addLast(element);
+            // 插入新桶, 保证升序
+            previousBucket.nextLink(nextBucket);
+            if (last == previousBucket) {
+                last = nextBucket;
+            }
+            // bucketMap添加记录
+            bucketMap.put(element.operations, nextBucket);
+        }
+        if (previousBucket.onlyOne()) {
+            // 如果桶中只有一个元素, 直接删除该桶
+            removeBucket(previousBucket);
+        } else {
+            // 从桶中移除该元素
+            previousBucket.remove(element);
+        }
     }
 
     protected void removeBucket(Bucket<K, V> bucket) {
